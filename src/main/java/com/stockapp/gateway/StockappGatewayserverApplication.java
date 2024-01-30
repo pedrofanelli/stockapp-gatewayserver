72440dee30915b7b2e493a6cc38e491bca8a4241
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-30 19:20:19 -0300

    init

---
+++ src/main/java/com/stockapp/gateway/StockappGatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.stockapp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockappGatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockappGatewayserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockapp/gateway/StockappGatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockappGatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
